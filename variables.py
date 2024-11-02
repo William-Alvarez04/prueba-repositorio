c28d8204b242d8372639a93c471564c4e7470b61
Willliam Alvarez <wilyalvarez1@gmail.com>
2024-11-02 00:59:47 -0600

    Subiendo el archivo de python

---
+++ variables.py
@@ -0,0 +1,18 @@
+
+#Definiendo una variable con camel Case
+nombreCompletoDeTuTioMaster = "Lucas Dalto"
+
+#Definiendo una variable con snake_case
+nombre_completo_de_tu_tio_master = "Lucas Dalto"
+
+nombre= "Lucas Dalto"
+
+#concatenar con +
+bienvenida = "Hola " + "¿Como estas?"
+
+#concatenar con f-strings
+bienvenida = f"Hola {nombre} ¿Como estas?"
+
+#operadores de pertenencia (in / not in)
+print("Lucas" in bienvenida) #true
+print("Lucas" not in bienvenida) #false5
